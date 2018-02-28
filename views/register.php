5e6b38c2689d7998ff6d449f5a9a4ff336b90360
Edward Ernesto Guevara Berríos <edwardgvr414@gmail.com>
2018-02-28 12:40:42 -0600

    Login y registro

--- views/register.php
+++ views/register.php
@@ -8,28 +8,31 @@
 <body>
 	<div class="form_register">
 		<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>">
-			<span>Iniciar Sesi&oacute;n</span>
+			<span>Registrarse</span>
 			<hr>
 			
 			<div class="nombre_completo">
 				<label for="user">Nombres:</label>
-				<input type="text" name="user_names" id="user_names" placeholder="Nombres">
-				<label for="user">Apellidos:</label>
-				<input type="text" name="user_lastnames" id="user_lastnames" placeholder="Apellidos">	
+				<div class="inputs_name">
+					<input type="text" name="user_names" id="user_names" placeholder="Nombres">
+					<input type="text" name="user_lastnames" id="user_lastnames" placeholder="Apellidos">	
+				</div>
 			</div>
 			
-			<label for="user">Usuario / email:</label>
-			<input type="text" name="user" id="user_login" placeholder="username / user@example.com">
+			<label for="user">Usuario:</label>
+			<input type="text" name="user" id="user_login" placeholder="username">
+			<label for="user">Correo:</label>
+			<input type="text" name="user" id="user_login" placeholder="user@example.com">
 			<label for="pass">Contrase&ntilde;a:</label>
 			<input type="password" name="pass" id="pass_login" placeholder="**********">
-			<label for="pass">Contrase&ntilde;a:</label>
+			<label for="pass">Repetir contrase&ntilde;a:</label>
 			<input type="password" name="pass" id="pass_login" placeholder="**********">
 			<div class="register_options">
 				<div class="login">
 					<span>Ya tengo cuenta.</span>
 					<a href="login.php">Iniciar Sesi&oacute;n</a>
 				</div>
-				<input type="submit" name="acces" value="Acceder">
+				<input type="submit" name="register" value="Registro">
 			</div>
 		</form>
 	</div>
