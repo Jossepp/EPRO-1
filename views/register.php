eff0135e1b330f5453e2dc15d13ad6ff769df281
Edward Ernesto Guevara Berríos <edwardgvr414@gmail.com>
2018-02-27 22:07:10 -0600

    Register

--- views/register.php
+++ views/register.php
@@ -2,22 +2,32 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Iniciar Sesi&oacute;n</title>
+	<title>Registro</title>
 	<link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-	<div class="form_login">
+	<div class="form_register">
 		<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>">
 			<span>Iniciar Sesi&oacute;n</span>
 			<hr>
+			
+			<div class="nombre_completo">
+				<label for="user">Nombres:</label>
+				<input type="text" name="user_names" id="user_names" placeholder="Nombres">
+				<label for="user">Apellidos:</label>
+				<input type="text" name="user_lastnames" id="user_lastnames" placeholder="Apellidos">	
+			</div>
+			
 			<label for="user">Usuario / email:</label>
 			<input type="text" name="user" id="user_login" placeholder="username / user@example.com">
 			<label for="pass">Contrase&ntilde;a:</label>
 			<input type="password" name="pass" id="pass_login" placeholder="**********">
-			<div class="login_options">
-				<div class="register">
-					<span>No tengo cuenta.</span>
-					<a href="register.php">Registrarse</a>
+			<label for="pass">Contrase&ntilde;a:</label>
+			<input type="password" name="pass" id="pass_login" placeholder="**********">
+			<div class="register_options">
+				<div class="login">
+					<span>Ya tengo cuenta.</span>
+					<a href="login.php">Iniciar Sesi&oacute;n</a>
 				</div>
 				<input type="submit" name="acces" value="Acceder">
 			</div>
